34e765408f39d8796c83c5d8a7ca7aaed3d4f428
gabriel mandler <37710821+GabrielMandler@users.noreply.github.com>
2019-07-03 13:53:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,29 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "Hello JavaScript!"
 }
 
-var banner = new AFBanner();
-var settings = {
-    // banner settings
-    title: "AppsFlyer",
-    subtitle: "Track campaigns on the go",
-    app_icon: "img/app_icon.png",
-    call_to_action: "Install",
-    show_only_mobile: true,
+// var banner = new AFBanner();
+// var settings = {
+//     // banner settings
+//     title: "AppsFlyer",
+//     subtitle: "Track campaigns on the go",
+//     app_icon: "img/app_icon.png",
+//     call_to_action: "Install",
+//     show_only_mobile: true,
     
-    // attribution settings
-    media_source: "banner_pid",
-    campaign: "banner_c",
-    adset: "banner_adset",
-    adset_id: "banner_adset_id",
-    ad: "banner_ad",
-    ad_id: "banner_ad_id",
-    site_id: "banner_site_id",
-    sub1: "banner_sub1",
+//     // attribution settings
+//     media_source: "banner_pid",
+//     campaign: "banner_c",
+//     adset: "banner_adset",
+//     adset_id: "banner_adset_id",
+//     ad: "banner_ad",
+//     ad_id: "banner_ad_id",
+//     site_id: "banner_site_id",
+//     sub1: "banner_sub1",
     
-    // routing settings
-    onelink_id: "R7ji",
-    subdomain: "appsflyer",
-    mobile_deeplink: "appsflyer://"
-};
+//     // routing settings
+//     onelink_id: "R7ji",
+//     subdomain: "appsflyer",
+//     mobile_deeplink: "appsflyer://"
+// };
 
-banner.init("my-banner", settings);
+// banner.init("my-banner", settings);
